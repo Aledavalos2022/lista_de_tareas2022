32ca1184ac949da5362fd6ba0f55a88713034dfa
alejandro <agdavalos14@gmail.com>
2022-05-19 18:48:06 -0300

    lista de items

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "../Task"
+
+export const TASKS: Task[] = [
+{
+    id:1,
+    text: "Terminar primer modulo de Angular",
+    day: 'Agosto 20 a las 17:00',
+    reminder: true
+},
+{
+    id:2,
+    text: "Hacer las compras para la cena",
+    day: 'Agosto 2 a las 18:00',
+    reminder: true
+},
+{
+    id:3,
+    text: "Investigar sobre Boostrap",
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+},
+{
+    id:4,
+    text: "Leer un libro",
+    day: 'Agosto 30 a las 12:00',
+    reminder: true
+}
+
+]
